9806634b095679dce234f0f79acefe7a80d0eb68
k0lan4ik <kolasynnikolai@gmail.com>
2024-04-10 13:53:51 +0300

    Добавленно управление базой данных

--- BlazorApp3/BlazorApp3/Program.cs
+++ BlazorApp3/BlazorApp3/Program.cs
@@ -1,12 +1,15 @@
 using BlazorApp3.Client.Pages;
 using BlazorApp3.Components;
+using BlazorApp3.Components.Pages;
+using DataLibrary;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveWebAssemblyComponents();
-
+builder.Services.AddSingleton<IDataAccess, DataAccess>();
+builder.Services.AddControllers();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -20,6 +23,8 @@ else
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+app.MapControllers();
+
 
 app.UseHttpsRedirection();
 
@@ -27,7 +32,8 @@ app.UseStaticFiles();
 app.UseAntiforgery();
 
 app.MapRazorComponents<App>()
-    .AddInteractiveWebAssemblyRenderMode()
+   .AddInteractiveWebAssemblyRenderMode()
     .AddAdditionalAssemblies(typeof(BlazorApp3.Client._Imports).Assembly);
 
 app.Run();
+
